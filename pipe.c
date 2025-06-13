1e4a22d886e6c17716554fed3ca16609b5e34024
k0ff33blu3 <milva.ricci@gmail.com>
2025-06-13 14:10:21 +0200

    pipes

--- pipe.c
+++ pipe.c
@@ -6,13 +6,21 @@
 /*   By: miricci <miricci@student.42firenze.it>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/12 15:52:14 by miricci           #+#    #+#             */
-/*   Updated: 2025/06/12 16:39:47 by miricci          ###   ########.fr       */
+/*   Updated: 2025/06/13 12:37:54 by miricci          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	create_pipe(char *cmd_line, int i, int size, char **envp, int (*pip)[2])
+void	close_pipe(int pip[2][2])
+{
+	close(pip[0][0]);
+	close(pip[0][1]);
+	close(pip[1][0]);
+	close(pip[1][1]);
+}
+
+void	create_pipe(char *cmd_line, int i, int size, char **envp, int pip[2][2])
 {
 	pid_t	pid;
 	t_cmdline	*data;
@@ -20,41 +28,48 @@ void	create_pipe(char *cmd_line, int i, int size, char **envp, int (*pip)[2])
 	// (void)fd;
 
 	data = data_init();
-	if (pipe(pip[i]) == -1)
-		perror("pipe");
 	pid = fork();
 	if (pid < 0)
-		perror("fork");
+		ft_error("fork");
 	if (pid == 0)
 	{
 		data_parsing(cmd_line, data);
-		// print_cmd_struct(*data, fd);
+		// print_cmd_struct(*data, 1);
 		if (i > 0 && !data->has_infile)
-			dup2(pip[i - 1][0], STDIN_FILENO);
+			dup2(pip[(i + 1) % 2][0], STDIN_FILENO);
 		if (i < size - 1 && !data->has_outfile)
-			dup2(pip[i][1], STDOUT_FILENO);
-		close(pip[i - 1][0]);
-		close(pip[i - 1][1]);
+			dup2(pip[i % 2][1], STDOUT_FILENO);
+		close(pip[(i + 1) % 2][0]);
+		close(pip[i % 2][1]);
 		exec_command(data, envp);
 	}
 	else
 	{
 		// wait(NULL);		//da togliere
-		close(pip[i - 1][0]);
-		close(pip[i - 1][1]);
+		if (i < size - 1)
+		{
+			close(pip[(i + 1) % 2][0]);
+			close(pip[(i + 1) % 2][1]);
+		}
 	}
 }
 
 void	piping(char **cmd_line, int size, char **envp)
 {
-	int	(*pip)[2];
+	int	pip[2][2];
 	int	i;
 
-	pip = malloc(sizeof(int[2]) * (size - 1));
-	if (!pip)
-		perror("malloc");
 	i = -1;
-	while (++i < size - 1)
+	while (++i < size)
+	{
+		if (i < size -1 && pipe(pip[i % 2]) == -1)
+			ft_error("pipe");
 		create_pipe(cmd_line[i], i, size, envp, pip);
-	free(pip);
+	}
+	// i = -1;
+// 	while (++i < size)
+// 	{
+// 		close(pip[i][0]);
+// 		close(pip[i][1]);
+// 	}
 }
