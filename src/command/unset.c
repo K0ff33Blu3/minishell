e9a1315d69052732198ebb617f5c1cd5f85eb843
k0ff33blu3 <milva.ricci@gmail.com>
2025-10-27 09:36:03 +0100

    unset funziona

--- src/command/unset.c
+++ src/command/unset.c
@@ -6,7 +6,7 @@
 /*   By: miricci <miricci@student.42firenze.it>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/05 12:26:24 by miricci           #+#    #+#             */
-/*   Updated: 2025/10/24 19:23:30 by miricci          ###   ########.fr       */
+/*   Updated: 2025/10/27 07:33:54 by miricci          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,25 +24,29 @@ int	unset(t_list **env_list, char **var)
 	int		i;
 	t_list	*node;
 	t_list	*tmp;
+	t_env	*env;
 
 	i = 1;
 	while (var[i])
 	{
 		len = ft_strlen(var[i]);
 		node = *env_list;
-		if (!ft_strncmp(var[i], (char *)node->content, len) && !ft_strncmp((char *)node->content + len, "=", 1))
+		if (!node)
+			return (perror("env_list"), 1);
+		env = (t_env *)node->content;
+		if (!ft_strncmp(var[i], env->name, len + 1))
 		{
 			*env_list = node->next;
-			free(node);
-			break ;
+			ft_lstdelone(node, free_env);
 		}
 		while (node->next)
 		{
-			if (!ft_strncmp(var[i], (char *)node->next->content, len) && !ft_strncmp((char *)node->next->content + len, "=", 1))
+			env = (t_env *)node->next->content;
+			if (!ft_strncmp(var[i], env->name, len + 1))
 			{
 				tmp = node->next;
 				node->next = tmp->next;
-				free(tmp);
+				ft_lstdelone(tmp, free_env);
 				break ;
 			}
 			node = node->next;
